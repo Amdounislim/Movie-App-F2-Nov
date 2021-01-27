85d1e80e0947913477938f79914ba18d25a501cf
Amdounislim <amdouni.slim@gmail.com>
2021-01-27 12:54:36 +0100

    first

---
+++ src/components/Rate.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Rate = ({ rating }) => {
+  let Stars = [];
+  for (let i = 0; i < 5; i++) {
+    if (i < rating) {
+      Stars.push(
+        <span className="rating" style={{ cursor: "pointer" }}>
+          ★
+        </span>
+      );
+    } else {
+      Stars.push(
+        <span className="rating" style={{ cursor: "pointer" }}>
+          ☆
+        </span>
+      );
+    }
+  }
+  return Stars;
+};
+
+export default Rate;
