c97cff64dcc3c047803e455563c3e1ecdddac1c1
Amdounislim <amdouni.slim@gmail.com>
2021-01-28 15:50:31 +0100

    second

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React, {useState} from "react"
-import './App.css';
-import {moviesData} from './components/MoviesData'
-import Main from './components/Main'
+import React, { useState } from "react";
+import "./App.css";
+import { moviesData } from "./components/MoviesData";
+import Main from "./components/Main";
+import Header from "./components/Header/Header";
 
 function App() {
-  const [movies, setMovies] = useState(moviesData)
+  const [movies, setMovies] = useState(moviesData);
+  const [search, setSearch] = useState("");
+  const [searchRate, setSearchRate] = useState(0);
 
-  const AddNewMovie=(x)=>{
-    setMovies([...movies, x])
-  }
+  const AddNewMovie = (x) => {
+    setMovies([...movies, x]);
+  };
 
   return (
     <div className="App">
-      <Main movies={movies} AddNewMovie={AddNewMovie}/>
+      <Header
+        setSearch={setSearch}
+        searchRate={searchRate}
+        setSearchRate={setSearchRate}
+      />
+      <Main
+        movies={movies.filter(
+          (el) =>
+            el.name.toLowerCase().includes(search.toLowerCase().trim()) &&
+            el.rating >= searchRate
+        )}
+        AddNewMovie={AddNewMovie}
+      />
     </div>
   );
 }
